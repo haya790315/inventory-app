efaaaa414de28f01a11b68df3da90e73414c58f0
ryk2025 <ruyo.ko@rightcode.co.jp>
2025-08-29 11:52:55 +0900

    merge origin/develop

--- inventory-bk/src/main/java/inventory/example/inventory_id/repository/CategoryRepository.java
+++ inventory-bk/src/main/java/inventory/example/inventory_id/repository/CategoryRepository.java
@@ -6,6 +6,7 @@ import java.util.Optional;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import java.util.List;
 
@@ -13,14 +14,29 @@ import java.util.List;
 public interface CategoryRepository extends JpaRepository<Category, UUID> {
   boolean existsByUserIdAndName(int userId, String name);
 
-  List<Category> findByUserIdIn(List<Integer> userIds);
+  @Query(value = """
+      SELECT *
+      FROM category
+      WHERE user_id IN (:userIds)
+      AND deleted_flag = FALSE
+      """, nativeQuery = true)
+  List<Category> findNotDeleted(List<Integer> userIds);
 
-  Optional<Category> findByUserIdAndName(int userId, String name);
+  @Query(value = """
+      SELECT *
+      FROM category
+      WHERE user_id IN (:userIds)
+      AND id = :id
+      AND deleted_flag = FALSE
+      """, nativeQuery = true)
+  Optional<Category> findUserCategory(List<Integer> userIds, UUID id);
 
-  Optional<Category> findByUserIdAndId(int userId, UUID id);
-
-  List<Category> findByUserIdInAndName(List<Integer> userIds, String name);
-
-  int countByUserIdAndDeletedFlagFalse(int userId);
-
+  @Query(value = """
+      SELECT *
+      FROM category
+      WHERE user_id IN (:userIds)
+      AND name = :name
+      AND deleted_flag = FALSE
+      """, nativeQuery = true)
+  List<Category> findActiveCateByName(List<Integer> userIds, String name);
 }
