31a3fe9fb4e3753396e8ff43275523005853125e
ryk2025 <ruyo.ko@rightcode.co.jp>
2025-10-07 13:17:25 +0900

    エラーメッセージのリネーム

--- inventory-bk/src/main/java/inventory/example/inventory_id/validation/CustomLocalDateDeserializer.java
+++ inventory-bk/src/main/java/inventory/example/inventory_id/validation/CustomLocalDateDeserializer.java
@@ -14,7 +14,7 @@ public class CustomLocalDateDeserializer extends JsonDeserializer<LocalDate> {
   private static final DateTimeFormatter EXPECTED_FORMAT = DateTimeFormatter
       .ofPattern("yyyy-MM-dd");
 
-  private static final String ERROR_MESSAGE = "有効期限の形式が不正です。yyyy-MM-dd形式で入力してください。";
+  private static final String ERROR_MESSAGE_DATE_FORMAT = "有効期限の形式が不正です。yyyy-MM-dd形式で入力してください。";
 
   @Override
   public LocalDate deserialize(JsonParser parser, DeserializationContext context) throws IOException {
@@ -24,13 +24,13 @@ public class CustomLocalDateDeserializer extends JsonDeserializer<LocalDate> {
       return null;
     }
     if (dateString.isEmpty()) {
-      throw new IllegalArgumentException(ERROR_MESSAGE);
+      throw new IllegalArgumentException(ERROR_MESSAGE_DATE_FORMAT);
     }
 
     try {
       return LocalDate.parse(dateString, EXPECTED_FORMAT);
     } catch (DateTimeParseException e) {
-      throw new IllegalArgumentException(ERROR_MESSAGE);
+      throw new IllegalArgumentException(ERROR_MESSAGE_DATE_FORMAT);
     }
   }
 }
